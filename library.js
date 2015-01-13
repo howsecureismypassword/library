4f733d156ceba9503866e35a23cdddf12f6302b3
Mark Wales <mark@smallhadroncollider.com>
2015-01-13 10:10:04 +0000

    updated library to use own currying function

--- library.js
+++ library.js
@@ -1,22 +1,70 @@
-var curry = require("curry");
+// From http://www.crockford.com/javascript/www_svendtofte_com/code/curried_javascript/index.html
+var curry = function (func, args, space) {
+    var n  = func.length - args.length; //arguments still to come
+    var sa = Array.prototype.slice.apply(args); // saved accumulator array
 
+    function accumulator(moreArgs, sa, n) {
+        var saPrev = sa.slice(0); // to reset
+        var nPrev  = n; // to reset
+
+        for(var i = 0; i < moreArgs.length; i++, n--) {
+            sa[sa.length] = moreArgs[i];
+        }
+
+        if ((n - moreArgs.length) <= 0) {
+            var res = func.apply(space, sa); // reset vars, so curried function can be applied to new params.
+            sa = saPrev;
+            n  = nPrev;
+            return res;
+        } else {
+            return function () {
+                // arguments are params, so closure bussiness is avoided.
+                return accumulator(arguments, sa.slice(0), n);
+            };
+        }
+    }
+
+    return accumulator([], sa, n);
+};
+
+/**
+ * Library functions
+ */
 var noOp = function () {};
 
+var isArray = function (val) {
+    return Object.prototype.toString.call( someVar ) === "[object Array]";
+};
+
+var isObject = function (val) {
+    return val !== null && typeof val === "object";
+};
+
+var isString = function (val) {
+    return typeof val == 'string' || val instanceof String;
+};
+
+var isNumber = function (val) {
+    return !isNaN(parseFloat(val)) && isFinite(val);
+};
+
 var forEach = curry(function (func, items) {
+    var i;
+
     items = items || [];
     func = func || noOp;
 
-    for (var i = 0, l = items.length; i < l; i++) {
-        func(items[i]);
+    if (isArray(items)) {
+        for (i = 0, l = items.length; i < l; i++) {
+            func(items[i]);
+        }
+    } else {
+        for (i in object) {
+            if (object.hasOwnProperty(i)) {
+                func(items[i]);
+            }
+        }
     }
-});
-
-var isString = curry(function (val) {
-    return typeof val == 'string' || val instanceof String;
-});
-
-var isNumber = curry(function (val) {
-    return !isNaN(parseFloat(val)) && isFinite(val);
 });
 
 var sortBy = curry(function (sortBy, arr) {
@@ -29,17 +77,17 @@ var prop = curry(function (prop, item) {
     return item[prop];
 });
 
-var toPairs = curry(function (object) {
+var toPairs = function (object) {
     var arr = [];
 
-    for (i in object) {
+    for (var i in object) {
         if (object.hasOwnProperty(i)) {
             arr.push([i, object[i]]);
         }
     }
 
     return arr;
-});
+};
 
 module.exports = {
     forEach: forEach,
